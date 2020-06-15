653c2a902ea2b7eb52378b2ffd185dc06c6b05fa
diegogm <diegogm@amazon.com>
2020-06-15 00:39:39 -0400

    new match defaults

--- sanityClientConfig.js
+++ sanityClientConfig.js
@@ -1,6 +1,6 @@
 export const sanityClientConfig = {
   // replace this id with the one you find in /studio/sanity.json
-  projectId: 'puj7p168',
+  projectId: 'cbaq8gxj',
   // make sure that the dataset name is the same as the one in /studio/sanity.json
   dataset: 'production',
   useCdn: false
--- studio/quizConfig.js
+++ studio/quizConfig.js
@@ -1,16 +1,16 @@
 export default {
   localWebHost: 'http://localhost:3000', // default Vue port
   localApiHost: 'http://localhost:3000', // default Now API port
-  remoteWebHost: '<your-app-name>.now.sh',
+  remoteWebHost: 'https://diegogm.vercel.app',
   match: {
     correctAnswerScore: 100,
     firstAnswerScore: 50
   },
   schema: {
-    maxAnswerLength: 40,
-    maxQuestionLength: 100,
+    maxAnswerLength: 300,
+    maxQuestionLength: 300,
     maxNumberOfChoices: 4,
     minNumberOfChoices: 2,
-    defaultTimeLimit: 30
+    defaultTimeLimit: 15
   }
 }
